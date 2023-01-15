59f65622238b41245395ef22434972ba738d5724
jongyunha <popawaw@naver.com>
2023-01-15 20:31:51 +0900

    fix linkedin url

--- blog-config.ts
+++ blog-config.ts
@@ -1,22 +1,22 @@
 const config = {
-  title: 'Tech Blog',     // Blog name
+  title: 'Tech Blog', // Blog name
   author: 'Jongyun', // Your name
   description: 'Kakaostyle',
   siteUrl: 'https://gatsby-starter-lavender.vercel.app',
-  thumbnail: '/thumbnails/jongyun.png',  // Default blog thumbnail. i.e. '/thumbnails/main.png'
+  thumbnail: '/thumbnails/jongyun.png', // Default blog thumbnail. i.e. '/thumbnails/main.png'
   social: {
-    github: 'jongyunha',   // GitHub username
-    twitter: '',        // Twitter username
-    facebook: '',       // Facebook username
-    linkedin: 'jonyun-ha-1997291b9',      // LinkedIn username
-    instagram: '',      // Instagram username
+    github: 'jongyunha', // GitHub username
+    twitter: '', // Twitter username
+    facebook: '', // Facebook username
+    linkedin: 'jongyun-ha-1997291b9', // LinkedIn username
+    instagram: '', // Instagram username
   },
   comment: {
     utterances: '', // Your GitHub repository
-    disqusShortName: '',  // Disqus short name
+    disqusShortName: '', // Disqus short name
   },
   icon: 'src/images/images.jpeg', // path of blog icon
-  googleAnalytics: '0',  // Google Analytics tracking id
+  googleAnalytics: '0', // Google Analytics tracking id
 };
 
 export default config;
